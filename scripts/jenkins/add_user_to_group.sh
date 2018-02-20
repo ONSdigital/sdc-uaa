7859c36911fbb00b812cab998636e88ee6d8cc37
Warren Bailey <warren@warrenbailey.net>
2018-02-20 08:39:47 +0000

    Fix add user to group

--- scripts/jenkins/add_user_to_group.sh
+++ scripts/jenkins/add_user_to_group.sh
@@ -2,5 +2,5 @@
 
 pipenv install requests
 
-pipenv run python ./scripts/add_user_to_group.py -ci admin -cs adminsecret -url uaa-$SPACE.apps.devtest.onsclofo.uk -g $GROUP_ID -u $USERNAME -v
+pipenv run python ./scripts/add_user_to_group.py -ci admin -cs adminsecret -url uaa-$SPACE.apps.devtest.onsclofo.uk -g surveys.$GROUP_ID -u $USERNAME -v
 
