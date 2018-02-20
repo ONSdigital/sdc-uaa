fdbb4ffed3bc3718eee1094e6d474d4f2c16f307
Warren Bailey <warren@warrenbailey.net>
2018-02-20 08:12:44 +0000

    Parameterize user details

--- scripts/jenkins/add_user.sh
+++ scripts/jenkins/add_user.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create Users
-pipenv run python add_users.py -ci login -cs loginsecret -url uaa-$SPACE.apps.devtest.onsclofo.uk -u $USERNAME -p $PASSWORD -e jimmy.mcgill@ons.gov.uk -f jimmy -l Bailey -v
+pipenv run python add_users.py -ci login -cs loginsecret -url uaa-$SPACE.apps.devtest.onsclofo.uk -u $USERNAME -p $PASSWORD -e $EMAIL -f $FIRSTNAME -l $LASTNAME -v
 
 # Query User
 pipenv run python query_users.py -ci admin -cs adminsecret -url uaa-$SPACE.apps.devtest.onsclofo.uk -u $USERNAME -p $PASSWORD -v
