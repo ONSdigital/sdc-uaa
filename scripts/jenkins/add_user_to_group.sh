9ebd1e137b60d80aedd6a0063446d264f74c5863
Warren Bailey <warren@warrenbailey.net>
2018-02-22 11:20:01 +0000

    Adding scripts for Jenkins (#11)
    Adding scripts for jenkins

---
+++ scripts/jenkins/add_user_to_group.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pipenv install requests
+
+pipenv run python ./scripts/add_user_to_group.py -ci admin -cs adminsecret -url uaa-$SPACE.apps.devtest.onsclofo.uk -g surveys.$GROUP_ID -u $USERNAME -v
+
--- scripts/test_setup.sh
+++ scripts/test_setup.sh
@@ -5,6 +5,9 @@
 # Create a client for RAS backstage
 pipenv run python create_client.py -a admin -s adminsecret -url uaa-int.apps.devtest.onsclofo.uk -c ras_backstage -p password -sc scim.me,scim.userids,073,074,user_attributes,profile -v
 
+# Create a client for Secure Message
+pipenv run python create_client.py -a admin -s adminsecret -url uaa-int.apps.devtest.onsclofo.uk -c secure_message -p password -sc scim.me,scim.userids,073,074,user_attributes,profile -v
+
 # Create Bricks groups
 pipenv run python create_group.py -ci login -cs loginsecret -url uaa-int.apps.devtest.onsclofo.uk -g 073 -d bricks -v
 
@@ -12,7 +15,7 @@ pipenv run python create_group.py -ci login -cs loginsecret -url uaa-int.apps.de
 pipenv run python create_group.py -ci login -cs loginsecret -url uaa-int.apps.devtest.onsclofo.uk -g 074 -d blocks -v
 
 # Create Users
-pipenv run python add_users.py -ci login -cs loginsecret -url uaa-int.apps.devtest.onsclofo.uk -u jimmy -p password -e jimmy.mcgill@ons.gov.uk -f jimmy -l Bailey -v
+pipenv run python add_users.py -ci login -cs loginsecret -url uaa-int.apps.devtest.onsclofo.uk -u jimmy -p password -e jimmy.mcgill@ons.gov.uk -f jimmy -l mcgill -v
 
 # Query User
 pipenv run python query_users.py -ci admin -cs adminsecret -url uaa-int.apps.devtest.onsclofo.uk -u jimmy -p password -v
