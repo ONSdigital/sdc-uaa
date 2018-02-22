5918b0190ef7a5f1fd812594858266f60df5d935
Gavin Edwards <gavin.edwards@ons.gsi.gov.uk>
2018-02-22 14:48:24 +0000

    Parameterise UAA URL

--- scripts/jenkins/add_client.sh
+++ scripts/jenkins/add_client.sh
@@ -3,4 +3,4 @@
 pipenv install requests
 
 # Create a client for RAS backstage
-pipenv run python ./scripts/create_client.py -a admin -s adminsecret -url uaa-$SPACE.apps.devtest.onsclofo.uk -c $CLIENT -p $PASSWORD -sc scim.me,scim.userids,surveys.073,surveys.074,user_attributes,profile -v
+pipenv run python ./scripts/create_client.py -a admin -s adminsecret -url $UAA_URL -c $CLIENT -p $PASSWORD -sc scim.me,scim.userids,surveys.073,surveys.074,user_attributes,profile -v
