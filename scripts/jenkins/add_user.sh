383d630e9c5dd4cf57ef8e94f02badc84a419039
Gavin Edwards <gavin.edwards@ons.gsi.gov.uk>
2018-02-27 10:30:01 +0000

    Amend the params used in shell scripts

--- scripts/jenkins/add_user.sh
+++ scripts/jenkins/add_user.sh
@@ -3,7 +3,7 @@
 pipenv install requests
 
 # Create Users
-pipenv run python ./scripts/add_users.py -ci admin -cs $ADMIN_SECRET -url $UAA_URL -u $USERNAME -p $PASSWORD -e $EMAIL -f $FIRSTNAME -l $LASTNAME -v
+pipenv run python ./scripts/add_users.py -ci login -cs $LOGIN_SECRET -url $UAA_URL -u $USERNAME -p $PASSWORD -e $EMAIL -f $FIRSTNAME -l $LASTNAME -v
 
 # Query User
 pipenv run python ./scripts/query_users.py -ci admin -cs $ADMIN_SECRET -url $UAA_URL -u $USERNAME -p $PASSWORD -v
