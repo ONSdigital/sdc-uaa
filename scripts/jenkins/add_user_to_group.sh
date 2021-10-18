332adab6788bbea88398069299975c48e60ea1c4
Warren Bailey <warren@warrenbailey.net>
2021-10-18 15:27:00 +0100

    Tidying up the UAA repository (#58)
    * Removing cloudfoundry files
    
    * [ci skip] auto patch increment
    skip-checks: true
    
    * Remove the java build files, the docker image and update the github action to stop building the old image we no longer need
    
    * re add the google key
    
    * tidy readme and delete old jenkins scripts
    
    * add additional -D for logging
    
    * standardise the github actions workflows
    
    * auto patch increment
    
    * deleting old spinnaker pipelines these now exist in ras-rm-spinnaker
    
    * end point is health not healthz
    
    * Revert "end point is health not healthz"
    
    This reverts commit dfda350b70e18f9d2d2c03b527f8018155ce8679.
    
    Co-authored-by: ras-rm-pr-bot <matt.crocker+rasrm@gmail.com>

--- scripts/jenkins/add_user_to_group.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pip install --upgrade pip==18.0
-pipenv run pip install pip==18.0
-pipenv install requests
-
-pipenv run python ./scripts/add_user_to_group.py -ci admin -cs $ADMIN_SECRET -url $UAA_URL -g surveys.$GROUP_ID -u $USERNAME -v
-
--- scripts/jenkins/change_password.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pip install --upgrade pip==18.0
-pipenv run pip install pip==18.0
-pipenv install requests
-
-# Create Users
-pipenv run python ./scripts/change_user_password.py -a admin -as $ADMIN_SECRET -url $UAA_URL -u $USERID -p $PASSWORD  -v
